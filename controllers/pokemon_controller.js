c1e98e56236afa3b2799ea1892784f7f1e0809cb
MUI LE <tanmui1992@gmail.com>
2022-07-13 23:56:07 +0800

    fix error

--- controllers/pokemon_controller.js
+++ controllers/pokemon_controller.js
@@ -10,24 +10,26 @@ const controller = {
 
   showPokemon: (req, res) => {
     // get the pokemon id from route param
-    const pokemonId = req.params.pokemon_id;
+    const pokemonID = req.params.pokemon_id;
     // validate the pokemon id
-    console.log("1. pokemonId is:", pokemonId);
-    const idNumber = parseInt(pokemonId);
+    console.log("1. pokemonId is:", pokemonID);
+    const idNum = parseInt(pokemonID);
+
     let errMsg = "";
-    if (isNaN(idNumber)) errMsg = "Must be a number";
-    if (idNumber < 0 || idNumber > pokemon.length)
+    if (isNaN(idNum)) {
+      errMsg = "must be number";
+    }
+    if (idNum < 0 || idNum > pokemon.length) {
       errMsg = "must be within range";
+    }
     // get the pokemon with the id from the "database"
-    console.log("idNumber is: ", idNumber);
-    const pokemonData = pokemon[idNumber];
+    // console.log("idNumber is: ", idNumber);
+    const pokemonData = pokemon[idNum];
     // render the "show" ejs template
-    console.log(pokemonData.name);
+    console.log(errMsg);
     res.render("show.ejs", {
       errMsg: errMsg,
-      pokemonName:
-        pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1),
-      pokemonImg: pokemonData.img,
+      pokemonData,
       back: "/pokemon",
     });
   },
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const pokemonController = require("./controllers/pokemon_controller");
 app.set("view engine", "ejs");
 app.use(`/public`, express.static("public"));
 // app.use(express.static(__dirname + "/public"));
-const port = 3000;
+const port = 8000;
 
 //Pokemon Routes:
 app.get("/pokemon", pokemonController.listPokemon);
