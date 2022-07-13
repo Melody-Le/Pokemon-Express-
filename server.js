acebeb6c04412fa20b2ad3c847d3e87797edbae5
MUI LE <tanmui1992@gmail.com>
2022-07-13 23:29:43 +0800

    success to lin css into file

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express();
 const pokemonController = require("./controllers/pokemon_controller");
 
 app.set("view engine", "ejs");
-
+app.use(`/public`, express.static("public"));
+// app.use(express.static(__dirname + "/public"));
 const port = 3000;
 
 //Pokemon Routes:
